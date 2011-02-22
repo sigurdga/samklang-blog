9cd319548cc639fcfeee6d79517a2e6726feb053
Jørgen Svedal Bergquist <gismo@skrot.net>
2011-02-22 20:51:35 +0100

    new_entry view: check for post, validate

--- views.py
+++ views.py
@@ -9,8 +9,16 @@ from blog.forms import EntryForm
 
 def new_entry(request):
     """Create a new entry"""
-    entry = Entry()
-    form = EntryForm(instance=entry)
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.user = request.user
+            entry.save()
+            return HttpResponseRedirect('/')
+    else:
+        entry = Entry()
+        form = EntryForm(instance=entry)
     
     return render_to_response(
         'blog/new_entry.html', 
