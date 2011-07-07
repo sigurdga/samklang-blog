9f95d6853aa6f6b5b731cbf05b8ddaa797ce4a2a
Jørgen Svedal Bergquist <gismo@skrot.net>
2011-07-07 23:27:19 +0200

    template tag and widget for showing last entries

---
+++ s7n/blog/templatetags/blog_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from s7n.blog.models import Entry
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def latest_entries(context, number):
+    """get a list, latest, of the 'numbember' latest entries"""
+    latest = Entry.live.all()[:number]
+    context['latest_entries'] = latest
+    return ''
