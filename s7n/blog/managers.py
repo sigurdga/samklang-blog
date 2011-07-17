4bdbab7e6cec4d96348d511458fac1ead0defb6f
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-07-17 14:01:38 +0200

    This is really more safe, but will we see it?

--- s7n/blog/managers.py
+++ s7n/blog/managers.py
@@ -5,14 +5,14 @@ from datetime import datetime
 
 class LiveEntryManager(Manager):
     """Manager that returns posts which is set to be shown live"""
-    
+
     def get_query_set(self):
-        now = datetime.now()
+        now = datetime.now
         return super(LiveEntryManager, self).get_query_set().filter(pub_date__lt=now).filter(
                 Q(pub_enddate=None) | Q(pub_enddate__gt=now))
-                
-        
-    
+
+
+
     def for_user(self, user=None):
         if user and user.is_authenticated():
             return self.get_query_set().filter(Q(group=None) | Q(group__user=user))
