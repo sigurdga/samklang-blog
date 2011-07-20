c75da1c212b112e57f8db175a397392de4294c12
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-07-20 13:14:19 +0200

    try adding s7n package here too. this should fail in some way.

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 setup(
         name = 's7n-blog',
         version = "1a1",
-        packages = ['s7n.blog', 's7n.blog.migrations'],
+        packages = ['s7n', 's7n.blog', 's7n.blog.migrations'],
         package_data = {'s7n.blog': ['templates/blog/*.html', 'locale/*/LC_MESSAGES/django.*o']},
         )
