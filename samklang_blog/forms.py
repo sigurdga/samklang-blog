5b617641c76dc11d32e3efe8e08e2868746a77df
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-12-11 23:34:17 +0100

    Change field ordering

--- samklang_blog/forms.py
+++ samklang_blog/forms.py
@@ -1,10 +1,12 @@
 from django.forms import ModelForm
 from samklang_blog.models import Entry
+from samklang_utils.forms import MarkdownTextarea
 
 class EntryForm(ModelForm):
 
     class Meta:
         model = Entry
-        fields = ('title', 'pub_date', 'pub_enddate', 'group', 'body')
+        fields = ('title', 'body', 'group', 'pub_date', 'pub_enddate')
+        widgets = {'body': MarkdownTextarea()}
 
 
