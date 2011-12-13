9e8fd6b77a10196ce54eee4c93107408067a395f
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-12-13 22:06:36 +0100

    Update version and changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
         name='samklang-blog',
-        version="0.2.0",
+        version="0.2.1",
         author='Jørgen Bergquist',
         author_email='gismos@gmail.com',
         url='http://github.com/sigurdga/samklang-blog',
